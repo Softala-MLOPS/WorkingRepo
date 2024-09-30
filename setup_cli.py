ca1067982966abf12e0964871d1ad7efa2cd47d2
Tukkis <tukkis96@outlook.com>
2024-09-30 16:15:40 +0300

    finished cli

---
+++ setup_cli.py
@@ -0,0 +1,38 @@
+import os
+import typer
+import subprocess
+
+def main(name: str):
+    print(f"Hello {name}")
+    subprocess.run(f"mkdir -p data", shell=True, capture_output=True)
+    os.chdir("data")
+    subprocess.run(f'echo "data" > Readme.md', shell=True, capture_output=True)
+    os.chdir("../")
+    subprocess.run(f"mkdir -p docs", shell=True, capture_output=True)
+    os.chdir("docs")
+    subprocess.run(f'echo "docs" > Readme.md', shell=True, capture_output=True)
+    os.chdir("../")
+    subprocess.run(f"mkdir -p models", shell=True, capture_output=True)
+    os.chdir("models") 
+    subprocess.run(f'echo "models" > Readme.md', shell=True, capture_output=True)
+    os.chdir("../")
+    subprocess.run(f"mkdir -p notebook", shell=True, capture_output=True)
+    os.chdir("notebook")
+    subprocess.run(f'echo "notebook" > Readme.md', shell=True, capture_output=True)
+    os.chdir("../")
+    subprocess.run(f"mkdir -p src", shell=True, capture_output=True)
+    os.chdir("src")
+    subprocess.run(f'echo "src" > Readme.md', shell=True, capture_output=True)
+    os.chdir("../")
+    subprocess.run(f"mkdir -p tests", shell=True, capture_output=True)
+    os.chdir("tests")
+    subprocess.run(f'echo "tests" > Readme.md', shell=True, capture_output=True)
+    os.chdir("../")
+    subprocess.run(f'touch .gitignore', shell=True, capture_output=True)
+    subprocess.run(f'touch LICENSE', shell=True, capture_output=True)
+    subprocess.run(f'touch README.md', shell=True, capture_output=True)
+    subprocess.run(f'touch requirements', shell=True, capture_output=True)
+    subprocess.run([f"git", 'add', '.'])
+
+if __name__ == "__main__":
+    typer.run(main)
