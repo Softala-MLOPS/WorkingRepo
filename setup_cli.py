747790d08972b1af495a4994a9ab9ab8a3fdce7b
Alex Kiippa <ext-alex.1.kiippa@sok.fi>
2024-10-02 14:13:55 +0300

    Add function for checking the GH cli installation

--- setup_cli.py
+++ setup_cli.py
@@ -1,15 +1,32 @@
 import os
 import typer
 import subprocess
+import sys
+
+def check_gh_installed():
+    """Check if GitHub CLI is installed."""
+    try:
+        result = subprocess.run(["gh", "--version"], capture_output=True, text=True)
+        if result.returncode != 0:
+            typer.echo("GitHub CLI (gh) is not installed.")
+            sys.exit(1)
+    except FileNotFoundError:
+        typer.echo("GitHub CLI (gh) is not installed.")
+        sys.exit(1)
 
 def main():
 
+    print("Checking if GitHub CLI is installed...")
+    check_gh_installed()
+
+    print("Creating a new repository...")
     result = subprocess.run("gh auth status", shell=True, capture_output=True, text=True)
     if "Logged in to github.com account" not in result.stdout:
         subprocess.run("gh auth login", shell=True)
     subprocess.run('gh repo create cli-repo --public --description "Your repository description" --clone', shell=True)
     os.chdir("cli-repo")
 
+    print("Creating the repository structure...")
     subprocess.run(f"mkdir -p data", shell=True, capture_output=True)
     os.chdir("data")
     subprocess.run(f'echo "data" > Readme.md', shell=True, capture_output=True)
